90e03b2862e297894cb92fd7538f56ed737a0b2c
Gosia <gosiamaria.siwicka@gmail.com>
2021-11-24 17:49:29 +0000

    tidies up single category page

--- nc-games-reviews/src/Components/Category.js
+++ nc-games-reviews/src/Components/Category.js
@@ -6,9 +6,10 @@ import ReviewCard from "./ReviewCard"
 
 export default function Category(props){
     const { slug } = useParams();
-
     const [reviews, setReviews] = useState([]);
     const [isLoading, setIsLoading] = useState(true);
+    const [sort, setSort] = useState('created_at')
+    const [order, setOrder] = useState('desc')
 
     useEffect(() => {
         setIsLoading(true);
@@ -16,7 +17,16 @@ export default function Category(props){
         setReviews(reviews);
         setIsLoading(false);
     });
-    }, [slug]);
+    }, [slug, order]);
+
+    const handleSelect = (e) => {
+        e.preventDefault();
+        setSort(e.target.value);
+    }
+    const handleChange = (e) => {
+        e.preventDefault();
+        setOrder(e.target.value);
+    }
 
     if (isLoading) {
         return <p>...loading</p>;
@@ -24,8 +34,37 @@ export default function Category(props){
 
     return (
         <main>
-        <h1>{slug.toUpperCase()}</h1>
+          <h2 className="subTitle">{slug}</h2>
         <About slug={slug} />
+        <div className="sortBy">
+        <select className="sortByOptions" onChange={handleSelect}>
+          <option
+            key="sortByOptions selector"
+            value="sortByOptions selector"
+            defaultValue="sortByOptions"
+            disabled
+            selected
+          >
+            Sort by
+          </option>
+          <option key="created_at" value="created_at">date</option>
+          <option key="title" value="title">title</option>
+          <option key="votes" value="votes">votes</option>
+        </select>
+
+        <select className="orderOptions" onChange={handleChange}>
+        <option key="orderOptions selector"
+            value="orderOptions selector"
+            defaultValue="orderOptions"
+            disabled
+            selected
+          >
+            Order
+          </option>
+          <option key="asc" value="asc">Ascending</option>
+          <option key="desc" value="desc">Descending</option>
+        </select>
+      </div>
         {reviews.map((review) => {
             return <ReviewCard review={review} />
         })}

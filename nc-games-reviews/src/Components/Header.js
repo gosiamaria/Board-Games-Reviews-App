c8f7eee8ed5832baa06e9a926d14fd8bc0241f54
Gosia <gosiamaria.siwicka@gmail.com>
2021-11-24 17:51:06 +0000

    adds link to header

--- nc-games-reviews/src/Components/Header.js
+++ nc-games-reviews/src/Components/Header.js
@@ -1,10 +1,11 @@
 import { Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
 export default function App() {
   return (
     <header>
       <Link to="/" className="title-link">
-      <h1>Northcoders Games Reviews</h1>
+      <h1>NORTHCODERS GAMES REVIEWS</h1>
       </Link>
     </header>
   )

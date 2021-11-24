b753c7e904a28dbadfa3613230e91db527efec3e
Gosia <gosiamaria.siwicka@gmail.com>
2021-11-24 17:53:11 +0000

    adds sort by and order options

--- nc-games-reviews/src/Components/Reviews.js
+++ nc-games-reviews/src/Components/Reviews.js
@@ -5,17 +5,65 @@ import ReviewCard from "./ReviewCard"
 
 export default function Reviews() {
   const [reviews, setReviews] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [sort, setSort] = useState('created_at')
+  const [order, setOrder] = useState('desc')
+
 
   useEffect(() => {
-    getAllReviews().then((reviews) => {
+    setIsLoading(true);
+    getAllReviews(sort, order).then((reviews) => {
       setReviews(reviews);
+      setIsLoading(false);
     });
-  }, []);
+  },[order]);
+  
+  const handleSelect = (e) => {
+    e.preventDefault();
+    setSort(e.target.value);
+  }
+  const handleChange = (e) => {
+    e.preventDefault();
+    setOrder(e.target.value);
+  }
 
+  if (isLoading) {
+    return <p>...loading</p>;
+  }
+  
   return (
     <main>
+      <h2 className="subTitle">About Northcoders Games Reviews</h2>
       <About />
-      <p className="sortBy">sort by:</p>
+      <div className="sortBy">
+        <select className="sortByOptions" onChange={handleSelect}>
+          <option
+            key="sortByOptions selector"
+            value="sortByOptions selector"
+            defaultValue="sortByOptions"
+            disabled
+            selected
+          >
+            Sort by
+          </option>
+          <option key="created_at" value="created_at">date</option>
+          <option key="comment_count" value="comment_count">comment count</option>
+          <option key="votes" value="votes">votes</option>
+        </select>
+
+        <select className="orderOptions" onChange={handleChange}>
+        <option key="orderOptions selector"
+            value="orderOptions selector"
+            defaultValue="orderOptions"
+            disabled
+            selected
+          >
+            Order
+          </option>
+          <option key="asc" value="asc">Ascending</option>
+          <option key="desc" value="desc">Descending</option>
+        </select>
+      </div>
         <div className="display">
           {reviews.map((review) => {
             return <ReviewCard review={review} />

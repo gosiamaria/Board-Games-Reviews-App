150c24c9f34bcff89e0992872cf85fd8f39174d9
Gosia <gosiamaria.siwicka@gmail.com>
2021-11-24 17:53:45 +0000

    adds link to userPage, avatar and Logout button

--- nc-games-reviews/src/Components/UserCorner.js
+++ nc-games-reviews/src/Components/UserCorner.js
@@ -1,52 +1,16 @@
-import { UserContext } from "../context/user";
-import { useContext, useState, useEffect } from "react";
-// import { Link,  useNavigate } from "react-router-dom";
-import { getAllUsers } from "../utils/api";
+import { UserContext } from "../context/UserContext";
+import { useContext } from "react";
+import { Link } from "react-router-dom";
 
 export default function UserCorner() {
   const { currentUser, setCurrentUser } = useContext(UserContext);
-  const [users, setUsers ] = useState([])
-  
-  useEffect(() => {
-    getAllUsers().then((users) => {
-      setUsers(users);
-    });
-  }, []);
-
-  const handleSelect = (e) => {
-    e.preventDefault();
-    setCurrentUser(e.target.value)
-  };
-
-console.log(currentUser);
-
-  if (currentUser.length === 0) {
-    return (
-      <div className="userCorner">
-        <select className="userOptions" onChange={handleSelect}>
-          <option
-            key="user-selector"
-            value="user-selector"
-            defaultValue="Select user"
-            disabled
-            selected
-          >
-            Select user
-          </option>
-          {users.map((user) => {
-            return (
-              <option key={user.username} value={user.username}>
-                {user.username}
-              </option>
-            );
-          })}
-      </select>
-      </div>
-    )
-  }
   return (
     <div className="userCorner">
-      <h2>{currentUser.username}</h2>
+      <Link to={`/users/${currentUser.username}`} className="userLink">
+    <p> {currentUser.username}</p>  </Link>
+    <Link to={`/users/${currentUser.username}`}><img src={currentUser.avatar_url} alt={currentUser.username} />
+    </Link>
+    <button onClick={() => setCurrentUser({})}>Logout</button>
     </div>
   )
 }
